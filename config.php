8711686877da770a0a8bc8ba46d60cd5b686303e
Vince Linise <32978709+ecnivtwelve@users.noreply.github.com>
2020-10-26 14:45:54 +0100

    new version 1.02

--- config.php
+++ config.php
@@ -1,13 +1,16 @@
 <?php
-    // Configuration file for Portfowlio
-    $website_name = "Portfowlio Example"; // Website's name
+    // Configuration file for Portflowio
+    $website_name = "Portflowio Example"; // Website's name
     $website_description = "There's a nice website"; // Website's description
+    $website_theme = "default"; // Website's theme
+    $website_icon = "icon.png"; // Favicon location
 
     // Configuration de la base de données
     $database_host = "localhost"; // Database's host
-    $database_name = "portfowlio"; // Database's name
+    $database_name = "portflowio"; // Database's name
     $database_user = "root"; // User name for MySQL
     $database_password = ""; // Password for MySQL
+    $database_table = "content"; // Portflowio table
 
     $database = new PDO('mysql:host=' . $database_host . ';dbname=' . $database_name . ';charset=utf8', $database_user, $database_password);
 ?>
--- index.php
+++ index.php
@@ -1,36 +1,5 @@
 <?php
     include 'config.php';
-    $select_content = $database->query("SELECT * FROM content");
+    $select_content = $database->query("SELECT * FROM " . $database_table);
+    include 'themes/' . $website_theme . '.php';
 ?>
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title><?php echo $website_name; ?></title>
-        <meta name="description" content="description">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    </head>
-    <body style="display:flex;align-items:center;justify-content:center;">
-        <div id="page" style="padding:20px;width:600px;margin-top:20px;">
-            <h2><?php echo $website_name; ?></h2>
-            <p><?php echo $website_description; ?></p>
-
-            <div id="projects">
-            <?php while($selection = $select_content->fetch()) {?>
-                <div class="card" style="margin-top: 10px;">
-                    <div class="card-body">
-                        <h5 class="card-title"><?php echo $selection['title']; ?></h5>
-                        <p class="card-text"><?php echo $selection['description']; ?></p>
-                        <a href="<?php echo $selection['link']; ?>" class="btn btn-primary"><?php echo $selection['button-name']; ?></a>
-                    </div>
-                </div>
-            <?php } ?>
-            </div>
-
-            <p style="margin-top: 20px; margin-bottom: 20px; opacity:50%;">(C) 2020 portfowlio</p>
-        </div>
-    </body>
-</html>

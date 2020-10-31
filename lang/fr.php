a2ceeb95f5b5bdbe5df95b04777656b5fce69572
Vince Linise <32978709+ecnivtwelve@users.noreply.github.com>
2020-10-31 21:23:42 +0100

    french language capability (by @BlueShadows47 )

---
+++ lang/fr.php
@@ -0,0 +1,50 @@
+<?php
+
+    //Global
+    $lang_global_back = "Retour";
+
+    //login.php
+    $lang_login_wrong1 = "Oups... Mauvais mot de passe !";
+    $lang_login_wrong2 = "Nom d'utlisateur inconnu...";
+    $lang_login_form_mail = "Adresse email";
+    $lang_login_form_pass = "Mot de passe";
+    $lang_login_form_tip1 = "Ne t'inquiète pas, c'est entre toi et moi.";
+    $lang_login_form_tip2 = "Caches ton clavier ! Quelqu'un regarde...";
+    $lang_login_form_tip3 = "Pas de compte ? Crées-le dans config.php.";
+    $lang_login_form_validate = "Se connecter";
+
+    //manage.php
+    $lang_manage_title = "Gérer les cartes.";
+    $lang_manage_description = "Changez la façon dont les gens voient vos cartes.";
+    $lang_manage_btn_add = "Ajouter une carte";
+    $lang_manage_btn_gen = "Générer la table dans la base de donnée";
+    $lang_manage_btn_logout = "Déconnexion";
+    $lang_manage_table_id = "ID";
+    $lang_manage_table_title = "Titre";
+    $lang_manage_table_description = "Description";
+    $lang_manage_table_link = "Lien";
+    $lang_manage_table_btnname = "Nom du bouton";
+    $lang_manage_table_actions = "Actions";
+    $lang_manage_action_edit = "Modifier";
+    $lang_manage_action_delete = "Supprimer";
+
+    //add.php - edit.php
+    $lang_add_title = "Ajouter une carte au site";
+    $lang_edit_title1 = 'Modifier la carte "';
+    $lang_edit_title2 = '"';
+    $lang_add_description = "Ajoutes plus de contenu, tu sais...";
+    $lang_add_input_title = "Titre de la carte";
+    $lang_add_input_description = "Description de la carte";
+    $lang_add_input_link = "Lien de la carte";
+    $lang_add_input_btnname = "Nom du bouton";
+    $lang_add_input_add = "Ajouter";
+    $lang_add_input_edit = "Modifier";
+
+    //delete-c.php - make-c.php
+    $lang_del_sure = "Êtes vous sûr ?";
+    $lang_del_text = "Il n'y a aucun moyen de revenir en arrière.";
+    $lang_make_text = "Si la table existe déjà, tout le contenu sera écrasé.";
+    $lang_del_back = "Revenir en arrière";
+    $lang_make_btn = "Aller.";
+
+?>

88ee36caa5e2b158e96d798cde1d7c54225e6095
Vince Linise <32978709+ecnivtwelve@users.noreply.github.com>
2020-11-01 14:51:44 +0100

    added belgium's french

---
+++ lang/fr-be.php
@@ -0,0 +1,50 @@
+<?php
+
+    //Global
+    $lang_global_back = "Revenir en arrière";
+
+    //login.php
+    $lang_login_wrong1 = "Mot de passe incorrect.";
+    $lang_login_wrong2 = "Nom d'utilisateur inexistant.";
+    $lang_login_form_mail = "Adresse e-mail";
+    $lang_login_form_pass = "Mot de passe";
+    $lang_login_form_tip1 = "T'inquiète, c'est entre toi et moi.";
+    $lang_login_form_tip2 = "Cache ton clavier, on t'observe.";
+    $lang_login_form_tip3 = "Pas de compte ? Fais le dans config.php.";
+    $lang_login_form_validate = "Login";
+
+    //manage.php
+    $lang_manage_title = "Gérer vos cartes.";
+    $lang_manage_description = "Changez la façon dont les gens voient vos cartes.";
+    $lang_manage_btn_add = "Ajouter une carte";
+    $lang_manage_btn_gen = "Générer la table dans la base de données";
+    $lang_manage_btn_logout = "Se déconnecter";
+    $lang_manage_table_id = "Id";
+    $lang_manage_table_title = "Titre";
+    $lang_manage_table_description = "Description";
+    $lang_manage_table_link = "Lien";
+    $lang_manage_table_btnname = "Nom du bouton";
+    $lang_manage_table_actions = "Actions";
+    $lang_manage_action_edit = "Modifier";
+    $lang_manage_action_delete = "Supprimer";
+
+    //add.php - edit.php
+    $lang_add_title = "Ajouter une carte au site web";
+    $lang_edit_title1 = 'Modifier "';
+    $lang_edit_title2 = '"';
+    $lang_add_description = "Ajouter du contenu, tvois.";
+    $lang_add_input_title = "Titre de la carte";
+    $lang_add_input_description = "Titre de la description";
+    $lang_add_input_link = "Lien de la carte";
+    $lang_add_input_btnname = "Nom du bouton";
+    $lang_add_input_add = "Ajouter";
+    $lang_add_input_edit = "Modifier";
+
+    //delete-c.php - make-c.php
+    $lang_del_sure = "Êtes vous sûr ?";
+    $lang_del_text = "Vous ne pourrez plus revenir en arrière.";
+    $lang_make_text = "Si la table existe déjà, tout le contenu sera écrasé.";
+    $lang_del_back = "Retourner au manager";
+    $lang_make_btn = "Vas-y.";
+
+?>

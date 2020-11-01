30ec462537109fa3736a846563c76d3fb59ceab1
Vince Linise <32978709+ecnivtwelve@users.noreply.github.com>
2020-11-01 14:49:55 +0100

    added spannish

---
+++ lang/es.php
@@ -0,0 +1,50 @@
+<?php
+
+    //Global
+    $lang_global_back = "Volver";
+
+    //login.php
+    $lang_login_wrong1 = "Contraseña incorrecta.";
+    $lang_login_wrong2 = "Nombre de usuario desconocido.";
+    $lang_login_form_mail = "E-mail adress";
+    $lang_login_form_pass = "Contraseña";
+    $lang_login_form_tip1 = "No te preocupes, está entre tú y yo.";
+    $lang_login_form_tip2 = "Oculta tu teclado, alguien mirando.";
+    $lang_login_form_tip3 = "¿No tienes cuenta? Hazlo en config.php.";
+    $lang_login_form_validate = "Ingresar";
+
+    //manage.php
+    $lang_manage_title = "Administra tus tarjetas.";
+    $lang_manage_description = "Cambia la forma en que la gente ve tus tarjetas.";
+    $lang_manage_btn_add = "Añadir una tarjeta";
+    $lang_manage_btn_gen = "Generar la tabla en tu base de datos";
+    $lang_manage_btn_logout = "Cerrar sesión";
+    $lang_manage_table_id = "Id";
+    $lang_manage_table_title = "Título";
+    $lang_manage_table_description = "Descripción";
+    $lang_manage_table_link = "Enlace";
+    $lang_manage_table_btnname = "Nombre del botón";
+    $lang_manage_table_actions = "Acciones";
+    $lang_manage_action_edit = "Editar";
+    $lang_manage_action_delete = "Eliminar";
+
+    //add.php - edit.php
+    $lang_add_title = "Añadir una tarjeta a su sitio web";
+    $lang_edit_title1 = 'Editar el "';
+    $lang_edit_title2 = 'Tarjeta';
+    $lang_add_description = "Añadiendo más contenido, ya sabe.";
+    $lang_add_input_title = "Título de la tarjeta";
+    $lang_add_input_description = "Descripción de la tarjeta";
+    $lang_add_input_link = "Enlace de tarjeta";
+    $lang_add_input_btnname = "Nombre del botón";
+    $lang_add_input_add = "Añadir";
+    $lang_add_input_edit = "Editar";
+
+    //delete-c.php - make-c.php
+    $lang_del_sure = "¿Estás seguro?";
+    $lang_del_text = "No hay forma de volver atrás.";
+    $lang_make_text = "Si la tabla ya existe, todo el contenido será sobrescrito.";
+    $lang_del_back = "Volver a Administrar";
+    $lang_make_btn = "Adelante.";
+
+?>

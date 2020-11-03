d473559e9ab706c8b0a8f8972c3dd9c999480d56
Vince Linise <32978709+ecnivtwelve@users.noreply.github.com>
2020-11-03 19:37:19 +0100

    polish added

---
+++ lang/pl.php
@@ -0,0 +1,50 @@
+<?php
+
+    //Global
+    $lang_global_back = "Wróć";
+
+    //login.php
+    $lang_login_wrong1 = "Błędne hasło.";
+    $lang_login_wrong2 = "Nieznana nazwa użytkownika.";
+    $lang_login_form_mail = "E-mail adress";
+    $lang_login_form_pass = "Hasło";
+    $lang_login_form_tip1 = "Nie martw się, to między Tobą a mną.";
+    $lang_login_form_tip2 = "Ukryj klawiaturę, ktoś ogląda.";
+    $lang_login_form_tip3 = "Nie masz konta? Utwórz to w config.php.";
+    $lang_login_form_validate = "Logowanie";
+
+    //manage.php
+    $lang_manage_title = "Zarządzaj swoimi kartami.";
+    $lang_manage_description = "Zmień sposób, w jaki ludzie widzą Twoje karty.";
+    $lang_manage_btn_add = "Dodaj kartę";
+    $lang_manage_btn_gen = "Generuj tabelę w swojej bazie danych";
+    $lang_manage_btn_logout = "Wyloguj się";
+    $lang_manage_table_id = "Id";
+    $lang_manage_table_title = "Rozporządzenie Rady (EWG) nr 2658/87 z dnia 23 lipca 1987 r. w sprawie nomenklatury taryfowej i statystycznej oraz w sprawie Wspólnej Taryfy Celnej (Dz.U. L 256 z 7.9.1987, s. 1).";
+    $lang_manage_table_description = "Opis";
+    $lang_manage_table_link = "Link";
+    $lang_manage_table_btnname = "Nazwa przycisku";
+    $lang_manage_table_actions = "Akcje";
+    $lang_manage_action_edit = "Edytuj";
+    $lang_manage_action_delete = "Usuń";
+
+    //add.php - edit.php
+    $lang_add_title = "Dodaj kartę do swojej strony internetowej";
+    $lang_edit_title1 = 'Edytuj "';
+    $lang_edit_title2 = '" karta';
+    $lang_add_description = "Dodawanie większej zawartości wiadomości.";
+    $lang_add_input_title = "Tytuł karty";
+    $lang_add_input_description = "Opis karty";
+    $lang_add_input_link = "Link do karty";
+    $lang_add_input_btnname = "Nazwa przycisku";
+    $lang_add_input_add = "Dodaj";
+    $lang_add_input_edit = "Edytuj";
+
+    //delete-c.php - make-c.php
+    $lang_del_sure = "Czy jesteś pewien?";
+    $lang_del_text = "Nie ma możliwości cofnięcia się.";
+    $lang_make_text = "Jeśli tabela już istnieje, cała zawartość zostanie nadpisana.";
+    $lang_del_back = "Wróć do zarządzania";
+    $lang_make_btn = "Idź dalej.";
+
+?>

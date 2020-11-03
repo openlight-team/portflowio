8c60b154f2195e4e7bdc14e189424476498f71bb
Vince Linise <32978709+ecnivtwelve@users.noreply.github.com>
2020-11-03 19:36:32 +0100

    added german

---
+++ lang/ge.php
@@ -0,0 +1,50 @@
+<?php
+
+    //Global
+    $lang_global_back = "Zurück";
+
+    //login.php
+    $lang_login_wrong1 = "Falsches Passwort.";
+    $lang_login_wrong2 = "Unbekannter Benutzername.";
+    $lang_login_form_mail = "E-mail adress";
+    $lang_login_form_pass = "Passwort";
+    $lang_login_form_tip1 = "Keine Sorge, es ist zwischen Ihnen und mir.";
+    $lang_login_form_tip2 = "Verstecke deine Tastatur, jemand schaut.";
+    $lang_login_form_tip3 = "Kein Konto ? Machen Sie es in config.php.";
+    $lang_login_form_validate = "Anmelden";
+
+    //manage.php
+    $lang_manage_title = "Verwalte deine Karten.";
+    $lang_manage_description = "Ändern Sie die Art und Weise, wie Leute Ihre Karten sehen.";
+    $lang_manage_btn_add = "Karte hinzufügen";
+    $lang_manage_btn_gen = "Generieren Sie die Tabelle in Ihrer Datenbank";
+    $lang_manage_btn_logout = "Abmelden";
+    $lang_manage_table_id = "Id";
+    $lang_manage_table_title = "Titel";
+    $lang_manage_table_description = "Beschreibung";
+    $lang_manage_table_link = "Link";
+    $lang_manage_table_btnname = "Buttonname";
+    $lang_manage_table_actions = "Aktionen";
+    $lang_manage_action_edit = "Bearbeiten";
+    $lang_manage_action_delete = "Löschen";
+
+    //add.php - edit.php
+    $lang_add_title = "Karte zu Ihrer Webseite hinzufügen";
+    $lang_edit_title1 = 'Bearbeiten Sie die "';
+    $lang_edit_title2 = '" Karte';
+    $lang_add_description = "Fügen Sie mehr Inhalt, ya wissen.";
+    $lang_add_input_title = "Kartentitel";
+    $lang_add_input_description = "Kartenbeschreibung";
+    $lang_add_input_link = "Kartenlink";
+    $lang_add_input_btnname = "Buttonname";
+    $lang_add_input_add = "Neu";
+    $lang_add_input_edit = "Bearbeiten";
+
+    //delete-c.php - make-c.php
+    $lang_del_sure = "Bist du dir sicher?";
+    $lang_del_text = "Es gibt keine Möglichkeit zurückzukehren.";
+    $lang_make_text = "Wenn die Tabelle bereits existiert, werden alle Inhalte überschrieben.";
+    $lang_del_back = "Zurück zum Verwalten";
+    $lang_make_btn = "Gehen Sie voran.";
+
+?>
